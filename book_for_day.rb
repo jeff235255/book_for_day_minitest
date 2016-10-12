ee0eb400387e2526abb80056c0ff5aab17b72876
Jeff Nguyen <jeff235255@gmail.com>
2016-10-12 16:10:43 +0700

    Update tdd test for BookForDay class

--- book_for_day.rb
+++ book_for_day.rb
@@ -1,5 +1,33 @@
 class BookForDay
+  BOOKS = [
+    "[Ruby] The ruby programming language.",
+    "[Ruby] What is ruby language?",
+    "[Ruby] What is minitest?",
+    "[Ruby] Testing with Ruby.",
+    "[Ruby] Learn to program.",
+    "[Ruby] Ruby in a nutsheell.",
+    "[Ruby] Ruby for rails.",
+    "[Rails] Ruby on rails for beginer.",
+    "[Rails] How to use rspec in Ruby on rails?",
+    "[Rails] How to build a gem?",
+    "[Rails] Deploying in rails apps with capistrano gem.",
+    "[Rails] Build a simple blog with Ruby on rails." 
+  ]
+
   def book_name
-    "What is minitest?"
+    BOOKS.sample
+  end
+
+  def get_random_book_names_for_day(number)
+    BOOKS.sample number
+  end
+
+  def get_random_book_with_parameter_name(book_name)
+    BOOKS.shuffle.each do |book|
+      if book.include? book_name
+        return book
+      end
+    end
+    return ""
   end
 end 
--- book_for_day_test.rb
+++ book_for_day_test.rb
@@ -2,8 +2,39 @@ require 'minitest/autorun'
 require_relative 'book_for_day'
 
 class BookForDayTest < Minitest::Test
+  def setup
+    @book_for_day = BookForDay.new
+  end
+
   def test_return_a_book_name
-    book_for_day = BookForDay.new
-    assert book_for_day.book_name
+    assert @book_for_day.book_name
+  end
+  
+  def test_predefined_books_is_array
+    assert_kind_of Array, BookForDay::BOOKS
+  end
+
+  def test_predefined_books_is_not_empty
+    refute_empty BookForDay::BOOKS
+  end
+
+  def test_return_a_book_name_includes_predefined
+    assert_includes BookForDay::BOOKS, @book_for_day.book_name
+  end
+  
+  def test_get_random_5_book_names_for_day
+    assert_equal 5, @book_for_day.get_random_book_names_for_day(5).count
+  end
+  
+  def test_get_random_ruby_book_for_day_is_empty
+    assert_empty @book_for_day.get_random_book_with_parameter_name "not ruby"
+  end
+
+  def test_get_random_ruby_book_for_day_is_not_empty
+    refute_empty @book_for_day.get_random_book_with_parameter_name "[Ruby]"
+  end
+
+  def test_get_random_rails_book_for_day_is_not_empty
+    refute_empty @book_for_day.get_random_book_with_parameter_name "[Rails]"
   end
 end
